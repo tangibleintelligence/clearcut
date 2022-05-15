ed45cc55345781e85d8cf8df509e198be4fd9d6a
Austin Howard <austin@tangibleintelligence.com>
2022-05-15 12:51:59 -0500

    more OTLP helper code

--- src/clearcut/__init__.py
+++ src/clearcut/__init__.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from opentelemetry.trace import Tracer
 
 from clearcut.logutils import log_block, get_logger
-from clearcut.otlputils import get_tracer, context_from_carrier, set_attribute
+from clearcut.otlputils import get_tracer, context_from_carrier, set_attribute, spanner
 
 
 def get_logger_tracer(name) -> Tuple[logging.Logger, Tracer]:
--- src/clearcut/otlputils.py
+++ src/clearcut/otlputils.py
@@ -3,15 +3,19 @@ Inits the OTLP framework.
 
 TODO should we use the logger instrumenter?
 """
+import asyncio
 from contextlib import contextmanager
+from functools import wraps
+from typing import Callable, Optional
 
+import more_itertools
 from opentelemetry import propagate, trace, context
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.propagators.textmap import CarrierT
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.trace import Tracer
-from opentelemetry.util.types import AttributeValue
+from opentelemetry.util.types import AttributeValue, Attributes
 
 
 def init_tracer_provider():
@@ -34,6 +38,11 @@ def set_attribute(key: str, value: AttributeValue):
     trace.get_current_span().set_attribute(key, value)
 
 
+def add_event(name: str, attributes: Attributes = None):
+    """Add an event to the current span"""
+    trace.get_current_span().add_event(name, attributes)
+
+
 @contextmanager
 def context_from_carrier(headers_carrier: CarrierT):
     """Set up OTEL context, and clear it after exiting block"""
@@ -52,3 +61,42 @@ def carrier_from_context() -> CarrierT:
     carrier = {}
     propagate.inject(carrier)
     return carrier
+
+
+def _get_tracer_for_func(func: Callable):
+    """
+    Get a tracer for the given function.
+    """
+    # Get a tracer. Ideally from the function's module...
+    tracer: Optional[Tracer] = more_itertools.first([v for v in func.__globals__.values() if isinstance(v, Tracer)], None)
+
+    # ...but otherwise, a new one with the function's module's name.
+    if tracer is None:
+        tracer = get_tracer(func.__module__)
+
+    return tracer
+
+
+def spanner(func: Callable):
+    """
+    Decorator that wraps the function as a span, with the function's name. Can also decorate async def functions (coroutine functions.)
+    """
+
+    @wraps(func)
+    async def with_span_async(*args, **kwargs):
+        span_name = f"{func.__module__}.{func.__name__}"
+        tracer = _get_tracer_for_func(func)
+        with tracer.start_as_current_span(span_name):
+            return await func(*args, **kwargs)
+
+    @wraps(func)
+    def with_span_sync(*args, **kwargs):
+        span_name = f"{func.__module__}.{func.__name__}"
+        tracer = _get_tracer_for_func(func)
+        with tracer.start_as_current_span(span_name):
+            return func(*args, **kwargs)
+
+    if asyncio.iscoroutinefunction(func):
+        return with_span_async
+    else:
+        return with_span_sync
